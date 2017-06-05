73a4a803bf2eb70db4dbc082ef8442ed17e07a61
Dane Petersen <thegreatsunra@gmail.com>
2017-06-05 15:30:30 -0500

    Add basic response to request for endpoint

--- endpoint.js
+++ endpoint.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+app.all('/', (req, res) => {
+  res.send('Two hundred okay!!!')
+})
 app.listen(3001, () => {
   console.log('Express endpoint listening on 3001')
 })
