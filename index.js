e1c79e596b2e90bf7b09b6ad71a795866e923917
Dane Petersen <thegreatsunra@gmail.com>
2017-06-05 15:28:33 -0500

    Add interval loop with message object

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ app.use(express.static('public'))
 app.listen(3000, () => {
   console.log('Express listening on port 3000')
 })
+
+let interval
+let count = 0
+
+interval = setInterval (() => {
+  count++
+
+  let now = new Date()
+  let timestamp = now.toISOString()
+  let message = JSON.stringify({
+    timestampUTC: timestamp,
+    messageCount: count,
+    payload: {
+      keyOne: true,
+      keyTwo: 'value'
+    }
+  })
+}, 1000)
