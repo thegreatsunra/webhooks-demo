ece591f8d30dfb7f0300c60ce63b948368141633
Dane Petersen <thegreatsunra@gmail.com>
2017-06-05 15:30:56 -0500

    Log the post body sent from the messaging server

--- endpoint.js
+++ endpoint.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.all('/', (req, res) => {
   res.send('Two hundred okay!!!')
+  console.log(`${JSON.stringify(req.body)}\nResponded to request!\n\n`)
 })
+
 app.listen(3001, () => {
   console.log('Express endpoint listening on 3001')
 })
